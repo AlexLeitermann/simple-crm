d1f821b7fd8db5643bb67c44a02eeaf19235a14f
Alexander Leitermann <alexander@leitermann-home.de>
2024-06-02 21:02:37 +0200

    Design - AllUsers-Table, Function - observe users

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
     MatDialogActions,
@@ -16,6 +16,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { User } from '../../models/user.class';
 import { addDoc, collection, Firestore, FirestoreModule } from '@angular/fire/firestore';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-dialog-add-user',
@@ -31,14 +32,18 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
         MatDatepickerModule,
         FirestoreModule,
         MatProgressBarModule,
+        CommonModule,
     ],
     providers: [],
     templateUrl: './dialog-add-user.component.html',
     styleUrl: './dialog-add-user.component.scss'
 })
 export class DialogAddUserComponent {
+    constructor(
+        public dialogRef: MatDialogRef<DialogAddUserComponent>
+    ) {}
 
-    loading = true;
+    loading = false;
     user = new User();
     birthDate = new Date;
 
@@ -48,15 +53,27 @@ export class DialogAddUserComponent {
     async saveUser() {
         this.loading = true;
         this.user.birthDate = this.birthDate.getTime();
-        await addDoc(collection(this.fs, 'users'), this.user.toJSON())
+        await addDoc(collection(this.fs, 'users'), this.userToJSON())
             .catch(
                 (err) => { console.error(err) }
             ).then((docRef) => { 
                 this.loading = false;
-                
                 console.log('user', this.user); 
+                this.dialogRef.close();
             }
         )
+    }
+
+    userToJSON() {
+        return {
+            firstName: this.user.firstName,
+            lastName: this.user.lastName,
+            birthDate: this.user.birthDate,
+            address: this.user.address,
+            zipCode: this.user.zipCode,
+            city: this.user.city,
+            email: this.user.email,
+        }
     }
 
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../../models/user.class';
-import { FirestoreModule } from '@angular/fire/firestore';
+import { collection, collectionData, Firestore, FirestoreModule, onSnapshot } from '@angular/fire/firestore';
+import { MatCardModule } from '@angular/material/card';
+import { Observable } from 'rxjs';
+
+interface Item {
+    firstName: string,
+    lastName: string,
+    birthDate: number,
+    address: string,
+    zipCode: number,
+    city: string,
+    email: string
+}
 
 @Component({
     selector: 'app-user',
@@ -18,19 +30,31 @@ import { FirestoreModule } from '@angular/fire/firestore';
         MatTooltipModule,
         MatDialogModule,
         FirestoreModule,
+        MatCardModule,
     ],
     templateUrl: './user.component.html',
     styleUrl: './user.component.scss'
 })
 export class UserComponent {
     user = new User();
-  
-    constructor(public dialog: MatDialog) {}
-  
+    fs: Firestore = inject(Firestore);
+    items$: Observable<any[]>;
+    allUsers = [];
+
+    constructor(
+        public dialog: MatDialog
+    ) {
+        const aCollection = collection(this.fs, 'users');
+        this.items$ = collectionData(aCollection);
+    }
+
+
     openDialog(): void {
         const dialogRef = this.dialog.open(DialogAddUserComponent);
   
         dialogRef.afterClosed().subscribe(result => {
             console.log('The dialog was closed');
         });
-    }}
+    }
+
+}
